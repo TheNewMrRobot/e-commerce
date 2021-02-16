da1e6990ee0e783528b01b61f000cb604dd98ab0
Vignesh1010 <ishwaranmmm@gmail.com>
2021-02-16 19:05:05 +0530

    removed logger

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -4,7 +4,6 @@ import { Route } from "react-router-dom";
 import CollectionPage from '../Category/Category';
 
 const Shop = ({match})=> {
-    console.log(match)
         return (
             <div className='shop-page'>
                 <Route exact path={`${match.path}`} component={CollectionOverview}/>
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -4,7 +4,11 @@ import { logger } from "redux-logger";
 //import { composeWithDevTools } from "redux-devtools-extension";
 import { persistStore } from "redux-persist";
 
-const middlewares = [logger]
+const middlewares = []
+
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger)
+}
 export const store = createStore(rootReducer,applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
